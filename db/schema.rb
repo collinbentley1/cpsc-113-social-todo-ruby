bfd5eff41221bfc19dbf6218278426c8081fdfeb
collinbentley1 <collin.bentley@me.com>
2016-02-20 00:32:55 +0000

    Finalize

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219055625) do
+ActiveRecord::Schema.define(version: 20160219233043) do
+
+
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "text"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
